637a6b95881e653e3b0b5ba847ded2540e023b1d
tiyh <actiyh@163.com>
2019-05-30 22:30:11 +0800

    redis util

--- src/main/java/com/example/config/CacheConfig.java
+++ src/main/java/com/example/config/CacheConfig.java
@@ -52,22 +52,29 @@ public class CacheConfig extends CachingConfigurerSupport {
 		return factory; 
 		} 
 	@Bean 
-	public CacheManager cacheManager(RedisTemplate redisTemplate) { 
+	public CacheManager cacheManager(StringRedisTemplate redisTemplate) { 
 		RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate); 
 		// Number of seconds before expiration. Defaults to unlimited (0) 
-		cacheManager.setDefaultExpiration(10); //设置key-value超时时间 
+		cacheManager.setDefaultExpiration(20); //设置key-value超时时间 
 		return cacheManager; 
 	} 
 	@Bean 
-	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) { 
+	public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) { 
 		StringRedisTemplate template = new StringRedisTemplate(factory); 
 		setSerializer(template); //设置序列化工具，这样ReportBean不需要实现Serializable接口 
 		template.afterPropertiesSet(); 
 		return template; 
 	} 
+	@Bean 
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) { 
+
+    RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+    template.setConnectionFactory(factory);
+	return template; 
+	} 
 	private void setSerializer(StringRedisTemplate template) { 
-		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = 
-				new Jackson2JsonRedisSerializer(Object.class);
+		Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = 
+				new Jackson2JsonRedisSerializer<Object>(Object.class);
 		ObjectMapper om = new ObjectMapper();
 		//om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
 		om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
